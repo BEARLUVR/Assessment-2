de8cac14188ab9ee7edc0301ed85dc221193e171
BEARLUVR <nicholas.glover3@education.nsw.gov.au>
2025-03-07 11:06:01 +1100

    added hashing,changed stuff with hashpassword.encode  and turns stuff into byte form with (utf-8) and saved password[0]

--- user_management.py
+++ user_management.py
@@ -1,28 +1,37 @@
 import sqlite3 as sql
 import time
 import random
+import bcrypt
 
-
+#user sign up to site,enters thier details into the SQL database
 def insertUser(username, password, DoB):
+    #convcerts to byte version of password
+    byte_password = password.encode('utf-8')
+    #hashes byte versionsS
+    hash_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
+
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
     cur.execute(
         "INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)",
-        (username, password, DoB),
+        (username, hash_password, DoB),
     )
     con.commit()
     con.close()
 
-
+#checks if user exists and checks thier password is corect
 def retrieveUsers(username, password):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
     cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
-    if cur.fetchone() == None:
+    if cur.fetchone() == None:    #this is checking the sql database if username exist,if not returns false to main
         con.close()
         return False
     else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
+        cur.execute(f"Select password FROM users WHERE username = '{username}'") 
+        #cur.execute(f"SELECT password FROM users WHERE username = '{username}'")
+        savedpassword = cur.fetchone()
+        print(savedpassword)
         # Plain text log of visitor count as requested by Unsecure PWA management
         with open("visitor_log.txt", "r") as file:
             number = int(file.read().strip())
@@ -31,7 +40,8 @@ def retrieveUsers(username, password):
             file.write(str(number))
         # Simulate response time of heavy app for testing purposes
         time.sleep(random.randint(80, 90) / 1000)
-        if cur.fetchone() == None:
+        byte_password = password.encode('utf-8')
+        if bcrypt.checkpw(byte_password, savedpassword[0]):
             con.close()
             return False
         else:
@@ -58,3 +68,4 @@ def listFeedback():
         f.write(f"{row[1]}\n")
         f.write("</p>\n")
     f.close()
+
